3ec0c28d013a07d4b10436ee75623da852de71d9
MoumitaRashidBushra <moumitarashidbushra@gmail.com>
2024-12-11 18:45:56 +0600

    Frontend fixed

--- common.js
+++ common.js
@@ -16,3 +16,31 @@ document.querySelectorAll(".menu a").forEach((link) => {
     link.classList.add("hover:bg-gray-200"); // Hover effect for inactive links
   }
 });
+
+// -----------------------------------------
+
+function toggleReserve() {
+  const reserveButton = document.getElementById("reserveButton");
+  if (reserveButton.classList.contains("btn-neutral")) {
+    reserveButton.classList.remove("btn-neutral", "text-white");
+    reserveButton.classList.add("btn-warning", "text-white");
+    reserveButton.textContent = "Stop Reserve to Daily Tank";
+  } else {
+    reserveButton.classList.add("btn-neutral", "text-white");
+    reserveButton.classList.remove("btn-warning", "text-white");
+    reserveButton.textContent = "Reserve to Daily Tank";
+  }
+}
+
+function toggleFeed() {
+  const feedButton = document.getElementById("feedButton");
+  if (feedButton.classList.contains("btn-neutral")) {
+    feedButton.classList.remove("btn-neutral", "text-white");
+    feedButton.classList.add("btn-warning", "text-white");
+    feedButton.textContent = "Stop Feed";
+  } else {
+    feedButton.classList.add("btn-neutral", "text-white");
+    feedButton.classList.remove("btn-warning", "text-white");
+    feedButton.textContent = "Feed Now";
+  }
+}
